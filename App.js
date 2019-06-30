40fb2b69c6926fca951ab453c3d8dafb793bf431
Paulo Salvatore <paulo@salvatore.tech>
2019-06-30 03:21:11 -0300

    Circle button rework

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import {Animated, Easing, ImageBackground, StyleSheet, View} from 'react-native';
-import CircleButton from 'react-native-circle-button';
+import {Animated, Easing, ImageBackground, StyleSheet, TouchableOpacity, View} from 'react-native';
+import iconAdd from './icons/add.png';
+import iconAttach from './icons/attach.png';
+import iconEmail from './icons/email.png';
+import iconPerson from './icons/person.png';
+import iconSetting from './icons/setting.png';
 import ViewShot from "react-native-view-shot";
 import {VLCPlayer} from 'react-native-vlcplayer';
+
+const size = 45;
 
 class SplashScreen extends React.Component {
     constructor() {
@@ -68,52 +74,226 @@ export default class App extends React.Component {
         }
     }
 
-    botaoCima = () => {
-        this.refs.viewShot.capture().then(uri => {
-            console.log("do something with ", uri);
-        });
+    onPressButtonTop = () => {
+        console.log('Click Top');
+        console.log(this._viewShot == undefined);
+        console.log(this.refs == undefined);
+        console.log(this.refs.viewShot == undefined);
+        // console.log(this.vlcPlayer);
+        // this.refs.viewShot.capture().then(uri => {
+        //     console.log("do something with ", uri);
+        // });
     };
 
-    botaoBaixo = () => {
+    onPressButtonBottom = () => {
         console.log('Click Baixo');
     };
 
-    botaoEsq = () => {
+    onPressButtonLeft = () => {
         console.log('Click Esquerda');
     };
 
-    botaoDir = () => {
+    onPressButtonRight = () => {
         console.log('Click Direita');
     };
 
+    scaleAnimated = new Animated.Value(0);
+    rotateAnimated = new Animated.Value(0);
+    fadeAnimated = new Animated.Value(0);
+    bringToLeftAnimated = new Animated.Value(0);
+    bringToRightAnimated = new Animated.Value(0);
+    bringToTopAnimated = new Animated.Value(0);
+    bringToBottomAnimated = new Animated.Value(0);
+
+    isClicked = false;
+
+    _buttonCenter = () => {
+        this.isClicked = !this.isClicked;
+
+        if (this.isClicked) {
+            this.startAnimation();
+        } else {
+            this.endAnimation();
+        }
+    };
+
+    _itemLeft = () => {
+        this.isClicked = false;
+
+        this.endAnimation();
+
+        this.onPressButtonLeft();
+    };
+
+    _itemRight = () => {
+        this.isClicked = false;
+
+        this.endAnimation();
+
+        this.onPressButtonRight();
+    };
+
+    _itemTop = () => {
+        this.isClicked = false;
+
+        this.endAnimation();
+
+        this.onPressButtonTop();
+    };
+
+    _itemBottom = () => {
+        this.isClicked = false;
+
+        this.endAnimation();
+
+        this.onPressButtonBottom();
+    };
+
+    startAnimation = () => {
+        this.createAnimation(this.rotateAnimated, 1, 200);
+        this.createAnimation(this.scaleAnimated, 1, 200);
+        this.createAnimation(this.fadeAnimated, 1, 100);
+        this.createAnimation(this.bringToLeftAnimated, 1, 200);
+        this.createAnimation(this.bringToRightAnimated, 1, 200);
+        this.createAnimation(this.bringToTopAnimated, 1, 200);
+        this.createAnimation(this.bringToBottomAnimated, 1, 200);
+    };
+
+    endAnimation = () => {
+        this.createAnimation(this.rotateAnimated, 2, 200);
+        this.createAnimation(this.scaleAnimated, 0, 200);
+        this.createAnimation(this.fadeAnimated, 0, 100);
+        this.createAnimation(this.bringToLeftAnimated, 0, 200);
+        this.createAnimation(this.bringToRightAnimated, 0, 200);
+        this.createAnimation(this.bringToTopAnimated, 0, 200);
+        this.createAnimation(this.bringToBottomAnimated, 0, 200);
+    };
+
+    createAnimation(obj, toValue, duration, easing) {
+        return Animated.timing(obj, {
+            toValue,
+            duration,
+            easing: Easing.linear,
+        }).start();
+    }
+
     render() {
+        const scaleMe = this.scaleAnimated.interpolate({
+            inputRange: [0, 1],
+            outputRange: [size, size * 2.8],
+        });
+
+        const rotateMe = this.rotateAnimated.interpolate({
+            inputRange: [0, 1, 2],
+            outputRange: ['0deg', '45deg', '0deg'],
+        });
+
+        const fadeInOut = this.fadeAnimated.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 1],
+        });
+
+        const bringMeToLeft = this.bringToLeftAnimated.interpolate({
+            inputRange: [0, 1],
+            outputRange: [size - 5, 2],
+        });
+
+        const bringMeToRight = this.bringToRightAnimated.interpolate({
+            inputRange: [0, 1],
+            outputRange: [size - 5, 2],
+        });
+
+        const bringMeToTop = this.bringToRightAnimated.interpolate({
+            inputRange: [0, 1],
+            outputRange: [size - 5, 2],
+        });
+
+        const bringMeToBottom = this.bringToRightAnimated.interpolate({
+            inputRange: [0, 1],
+            outputRange: [size - 5, 2],
+        });
+
         if (this.state.isLoading) {
             return <SplashScreen/>;
         }
 
         return (
-            <ViewShot ref="viewShot" style={{flex: 1}}>
-                <VLCPlayer
-                    ref={ref => (this.vlcPlayer = ref)}
-                    style={{marginTop: 0, height: "100%", width: "100%"}}
-                    source={{uri: 'rtsp://192.168.2.200:7070/webcam'}}/>
+            <View style={styles.appContainer}>
+                <ViewShot ref={component => this._viewShot = component} style={styles.viewShot}>
+                    <VLCPlayer
+                        ref={ref => (this.vlcPlayer = ref)}
+                        style={styles.vlcPlayer}
+                        source={{uri: 'rtsp://192.168.15.166/videodevice'}}/>
+                </ViewShot>
 
-                <View style={{
-                    position: 'absolute',
-                    right: 0,
-                    marginTop: 155,
-                    marginRight: 60,
-                    zIndex: 1,
-                    height: 50,
-                    width: 50,
-                    opacity: 0.8
-                }}>
-                    <CircleButton size={45} primaryColor='#4cb0e4' secondaryColor='#66c87d'
-                                  onPressButtonTop={this.botaoCima} onPressButtonBottom={this.botaoBaixo}
-                                  onPressButtonRight={this.botaoDir} onPressButtonLeft={this.botaoEsq}/>
+                <View style={styles.circleContainer}>
+                    <View style={styles.centerContainer}>
+                        <Animated.View style={[
+                            styles.baseCircle,
+                            {
+                                width: scaleMe,
+                                height: scaleMe
+                            }
+                        ]}>
+                        </Animated.View>
+                    </View>
+                    <View style={styles.viewCenter}>
+
+                        <Animated.View style={[styles.subItem, {left: bringMeToLeft}]}>
+                            <TouchableOpacity style={styles.subItem}
+                                              activeOpacity={1}
+                                              onPress={this._itemLeft}>
+                                <Animated.Image
+                                    source={iconAttach}
+                                    style={[styles.centerImage, {opacity: fadeInOut}]}
+                                />
+                            </TouchableOpacity>
+                        </Animated.View>
+
+                        <Animated.View style={[styles.subItem, {right: bringMeToRight}]}>
+                            <TouchableOpacity style={styles.subItem}
+                                              activeOpacity={1}
+                                              onPress={this._itemRight}>
+                                <Animated.Image
+                                    source={iconEmail}
+                                    style={[styles.centerImage, {opacity: fadeInOut}]}
+                                />
+                            </TouchableOpacity>
+                        </Animated.View>
+
+                        <Animated.View style={[styles.subItem, {top: bringMeToTop}]}>
+                            <TouchableOpacity style={styles.subItem}
+                                              activeOpacity={1}
+                                              onPress={this._itemTop}>
+                                <Animated.Image
+                                    source={iconPerson}
+                                    style={[styles.centerImage, {opacity: fadeInOut}]}
+                                />
+                            </TouchableOpacity>
+                        </Animated.View>
+
+                        <Animated.View style={[styles.subItem, {bottom: bringMeToBottom}]}>
+                            <TouchableOpacity style={styles.subItem}
+                                              activeOpacity={1}
+                                              onPress={this._itemBottom}>
+                                <Animated.Image
+                                    source={iconSetting}
+                                    style={[styles.centerImage, {opacity: fadeInOut}]}
+                                />
+                            </TouchableOpacity>
+                        </Animated.View>
+
+                        <TouchableOpacity style={styles.itemOpen}
+                                          activeOpacity={1}
+                                          onPress={this._buttonCenter}>
+                            <Animated.Image
+                                source={iconAdd}
+                                style={{transform: [{rotate: rotateMe}]}}
+                            />
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </ViewShot>
-
+            </View>
         );
     }
 }
@@ -123,19 +303,71 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#FFF'
+        backgroundColor: '#FFFFFF'
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10
+    appContainer: {
+        flex: 1
     },
-    myButton: {
-        marginTop: 510,
-        padding: 5,
-        height: 50,
-        width: 50,  //The Width must be the same as the height
-        borderRadius: 100 //Then Make the Border Radius twice the size of width or Height
+    centerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: size * 2.8,
+        height: size * 2.8
+    },
+    circleContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        // marginRight: size / 2
+    },
+    viewCenter: {
+        flex: 1,
+        position: 'absolute',
+        width: size * 2.8,
+        height: size * 2.8,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    itemOpen: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        width: 45,
+        height: 45,
+        backgroundColor: '#4CB0E4',
+        borderRadius: 360
+    },
+    baseCircle: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        width: 45,
+        height: 45,
+        backgroundColor: '#66C87D',
+        borderRadius: 360
+    },
+    centerImage: {
+        width: size - 10,
+        height: size - 10
+    },
+    subItem: {
+        flex: 1,
+        position: 'absolute',
+        width: size - 10,
+        height: size - 10
+    },
+    viewShot: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%'
+    },
+    vlcPlayer: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%'
     }
 });
 
--- android/app/src/main/java/com/healthapp/MainApplication.java
+++ android/app/src/main/java/com/healthapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.healthapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.yuanzhou.vlc.ReactVlcPlayerPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNViewShotPackage(),
             new ReactVlcPlayerPackage(),
             new RNCWebViewPackage(),
             new RNGestureHandlerPackage()
